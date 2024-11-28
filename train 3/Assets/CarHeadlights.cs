3c31ea63dd03d75a1a8770b74d5f509d37b4daff
Mohammadreza-Edrisabadi <75852598+Mohammadreza-Edrisabadi@users.noreply.github.com>
2024-11-28 10:09:55 +0330

    Added Unity project files

---
+++ train 3/Assets/CarHeadlights.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CarHeadlights : MonoBehaviour
+{
+    public Light[] headlights;
+    private bool lightsOn = false;
+    void Start()
+    {
+
+        foreach (Light light in headlights)
+        {
+            light.enabled = false;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            lightsOn = !lightsOn;
+
+            foreach (Light light in headlights)
+            {
+                light.enabled = lightsOn; 
+
+                if (lightsOn)
+                {
+                    light.color = Color.yellow;
+                }
+                else
+                {
+                    light.color = Color.white; 
+                }
+            }
+        }
+    }
+}
+    
+
---
+++ train 3/Assets/PoliceSiren.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceSiren : MonoBehaviour
+{
+    public Light redLight; 
+    public Light blueLight; 
+    public AudioSource sirenSound; 
+    private bool isSirenActive = false; 
+
+    public float waitTime = 0.2f;
+
+    void Start()
+    {
+        
+        redLight.enabled = false;
+        blueLight.enabled = false;
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isSirenActive = !isSirenActive;
+
+            if (isSirenActive)
+            {
+                StartCoroutine(Siren());
+                sirenSound.Play(); 
+            }
+            else
+            {
+                StopAllCoroutines();
+                redLight.enabled = false;
+                blueLight.enabled = false;
+                sirenSound.Stop(); 
+            }
+        }
+    }
+
+    IEnumerator Siren()
+    {
+        while (isSirenActive)
+        {
+            redLight.enabled = true;
+            blueLight.enabled = false;
+            yield return new WaitForSeconds(waitTime);
+
+            redLight.enabled = false;
+            blueLight.enabled = true;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
